3e0c1ce68531a6d246c4dde32efb5359bbffe426
Tiebo <1459847864@qq.com>
2023-04-21 14:11:51 +0800

    create project

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  //这里可以对tailwind的类名进行扩展
+  theme: {
+    extend: {
+      colors: {
+        //将 colors 类名替换为你的颜色
+        primary: "#ff0000",
+      },
+      zIndex: {
+        //在zindex当中添加你的自定义类名z-1
+        "-1": "-1",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+
+}
+
